e21ee01482c1109c2e9da233d62e3df1cd933f8f
longnguyen <nhatlong2356@gmail.com>
2024-03-23 16:21:29 +0700

    Update .circleci 1

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 DOCKER_COMPOSE_FILE="./docker-compose.yml"
 
-if ! command -v docker-compose &> /dev/null; then
+if ! command -v docker compose &> /dev/null; then
     echo "Lỗi: Docker Compose không được cài đặt hoặc không tìm thấy."
     exit 1
 fi
@@ -12,7 +12,7 @@ if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
     exit 1
 fi
 
-docker-compose down --volumes --remove-orphans
+docker compose down --volumes --remove-orphans
 docker image prune -f
-docker-compose pull
-docker-compose up -d
+docker compose pull
+docker compose up -d
